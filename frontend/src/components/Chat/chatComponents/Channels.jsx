7d32395af58147dfdfd3121813cc21e4f363becf
Ilia <149499467+mxclg@users.noreply.github.com>
2025-03-26 16:59:28 +0100

    test №11

--- frontend/src/components/Chat/chatComponents/Channels.jsx
+++ frontend/src/components/Chat/chatComponents/Channels.jsx
@@ -46,11 +46,11 @@ const Channels = () => {
         <b>{t('ui.channels')}</b>
         <Button className="p-0 text-primary" variant="group-vertical" onClick={() => handleOpenModal('addChannel')}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
-                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
-                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
-              </svg>
-              <span className="visually-hidden">+</span>
-          </Button>
+            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+          </svg>
+          <span className="visually-hidden">+</span>
+        </Button>
       </div>
       <div className="overflow-auto flex-grow-1">
         {loading ? (
@@ -61,34 +61,40 @@ const Channels = () => {
           <ul className="list-unstyled">
             {channels.map(channel => (
               <li key={channel.id} className="nav-item w-100">
-              <div className="d-flex justify-content-between align-items-center">
-                <Button
-                  variant={channel.id === currentChannelId ? 'secondary' : 'light'}
-                  className={`w-100 rounded-0 text-start text-truncate ${channel.id === currentChannelId ? 'btn' : ''}`}
-                  onClick={() => dispatch(changeChannel(channel.id))}
-                >
-                  # {clean(channel.name)}
-                </Button>
-                {!isSystemChannel(channel) && (
-                  <Dropdown as={ButtonGroup}>
-                  <Dropdown.Toggle
-                    split
-                    id={`dropdown-${channel.id}`}
+                <div className="d-flex justify-content-between align-items-center">
+                  <Button
                     variant={channel.id === currentChannelId ? 'secondary' : 'light'}
-                    className="rounded-0 border-start-0"
-                  />
-                  <Dropdown.Menu>
-                    <Dropdown.Item onClick={() => handleOpenRenameModal(channel)}>
-                      {t('buttons.rename')}
-                    </Dropdown.Item>
-                    <Dropdown.Item onClick={() => handleOpenRemoveModal(channel)}>
-                      {t('buttons.remove')}
-                    </Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-                )}
-              </div>
-            </li>
+                    className={`w-100 rounded-0 text-start text-truncate ${channel.id === currentChannelId ? 'btn' : ''}`}
+                    onClick={() => dispatch(changeChannel(channel.id))}
+                  >
+                    # {clean(channel.name)}
+                  </Button>
+                  {!isSystemChannel(channel) && (
+                    <Dropdown as={ButtonGroup}>
+                      <Button
+                        variant={channel.id === currentChannelId ? 'secondary' : 'light'}
+                        onClick={() => handleOpenRenameModal(channel)}
+                      >
+                        {t('ui.manageChannel')}
+                      </Button>
+                      <Dropdown.Toggle
+                        split
+                        id={`dropdown-${channel.id}`}
+                        variant={channel.id === currentChannelId ? 'secondary' : 'light'}
+                        className="rounded-0 border-start-0"
+                      />
+                      <Dropdown.Menu>
+                        <Dropdown.Item onClick={() => handleOpenRenameModal(channel)}>
+                          {t('buttons.rename')}
+                        </Dropdown.Item>
+                        <Dropdown.Item onClick={() => handleOpenRemoveModal(channel)}>
+                          {t('buttons.remove')}
+                        </Dropdown.Item>
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  )}
+                </div>
+              </li>
             ))}
           </ul>
         )}
